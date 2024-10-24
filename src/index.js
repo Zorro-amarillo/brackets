a1f566e5f38c40e130a938e1ee0f7459767625fa
Yana Malakhova <ya6ka0@gmail.com>
2024-10-25 02:07:12 +0300

    feat: implement check-function for given brackets sequence

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+
+  for (let i = 0; i < str.length; i += 1) {
+    const currentSymbol = str[i];
+
+    for (let arr of bracketsConfig) {
+      const topStackItem = stack[stack.length - 1];
+
+      // Special Case
+      if (currentSymbol === arr[0] && currentSymbol === arr[1]) {
+        if (currentSymbol === topStackItem) {
+          stack.pop();
+        } else {
+          stack.push(currentSymbol);
+        }
+      }
+
+      // Opening Bracket
+      if (currentSymbol === arr[0] && currentSymbol !== arr[1]) {
+        stack.push(currentSymbol);
+      }
+
+      // Closing Bracket
+      if (currentSymbol === arr[1] && currentSymbol !== arr[0]) {
+        if (stack.length === 0) {
+          return false;
+        } else {
+          if (topStackItem === arr[0]) {
+            stack.pop();
+          } else {
+            return false;
+          }
+        }
+      }
+
+    }
+  }
+  return stack.length === 0;
 }
